5439577d5acae2b75b2a3409e7bab538d8fc2b81
Victor H. García <victorhugogarcia13d@gmail.com>
2025-02-24 01:57:54 -0600

    GUNDI-4040: Include UI options to pull_events schema (sorting and UI options)

--- app/actions/configurations.py
+++ app/actions/configurations.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from app.actions.core import PullActionConfiguration, AuthActionConfiguration, ExecutableActionMixin
 from app.actions.gfwclient import AOIData, DatasetResponseItem, IntegratedAlertsConfidenceEnum, NasaViirsFireAlertConfidenceEnum
 from app.services.errors import ConfigurationNotFound
-from app.services.utils import find_config_for_action
+from app.services.utils import find_config_for_action, FieldWithUIOptions, UIOptions, GlobalUISchemaOptions
 
 
 class AuthenticateConfig(AuthActionConfiguration, ExecutableActionMixin):
@@ -60,19 +60,25 @@ class PullEventsConfig(PullActionConfiguration):
         description="Lowest confidence level to include in the connection."
     )
 
-    fire_lookback_days: int = pydantic.Field(
-        10,
+    fire_lookback_days: int = FieldWithUIOptions(
+        5,
         le=10,
         ge=1,
         title="Fire alerts lookback days",
-        description="Number of days to look back for fire alerts."
+        description="Number of days to look back for fire alerts.",
+        ui_options=UIOptions(
+            widget="range",  # This will be rendered ad a range slider
+        )
     )
-    integrated_alerts_lookback_days: int = pydantic.Field(
-        30,
+    integrated_alerts_lookback_days: int = FieldWithUIOptions(
+        15,
         le=30,
         ge=1,
         title="Integrated deforestation alerts lookback days",
-        description="Number of days to look back for integrated deforestation alerts."
+        description="Number of days to look back for integrated deforestation alerts.",
+        ui_options = UIOptions(
+            widget="range",  # This will be rendered ad a range slider
+        )
     )
 
     force_fetch: bool = pydantic.Field(
@@ -80,6 +86,91 @@ class PullEventsConfig(PullActionConfiguration):
         title="Force fetch",
         description="Force fetch even if in a quiet period."
     )
+
+    ui_global_options: GlobalUISchemaOptions = GlobalUISchemaOptions(
+        order=[
+            "gfw_share_link_url",
+            "include_fire_alerts",
+            "fire_lookback_days",
+            "fire_alerts_lowest_confidence",
+            "include_integrated_alerts",
+            "integrated_alerts_lookback_days",
+            "integrated_alerts_lowest_confidence",
+            "force_fetch"
+        ],
+    )
+
+    class Config:
+        @staticmethod
+        def schema_extra(schema: dict):
+            # Remove latitude, longitude, distance and region_code from the root properties
+            schema["properties"].pop("fire_alerts_lowest_confidence", None)
+            schema["properties"].pop("fire_lookback_days", None)
+            schema["properties"].pop("integrated_alerts_lookback_days", None)
+            schema["properties"].pop("integrated_alerts_lowest_confidence", None)
+
+            # Show region_code OR latitude & longitude & distance based on search_parameter
+            schema.update({
+                "allOf": [{
+                    "if": {
+                        "properties": {
+                            "include_fire_alerts": {
+                                "const": True
+                            }
+                        }
+                    },
+                    "then": {
+                        "required": ["fire_lookback_days", "fire_alerts_lowest_confidence"],
+                        "properties": {
+                            "fire_lookback_days": {
+                                "type": "integer",
+                                "title": "Fire alerts lookback days",
+                                "default": 5,
+                                "maximum": 10,
+                                "minimum": 1,
+                                "description": "Number of days to look back for fire alerts."
+                            },
+                            "fire_alerts_lowest_confidence": {
+                                "allOf": [{
+                                    "$ref": "#/definitions/NasaViirsFireAlertConfidenceEnum"
+                                }],
+                                "title": "Fire alerts lowest confidence",
+                                "default": "high",
+                                "description": "Lowest confidence level to include in the connection."
+                            }
+                        }
+                    }
+                }, {
+                    "if": {
+                        "properties": {
+                            "include_integrated_alerts": {
+                                "const": True
+                            }
+                        }
+                    },
+                    "then": {
+                        "required": ["integrated_alerts_lookback_days", "integrated_alerts_lowest_confidence"],
+                        "properties": {
+                            "integrated_alerts_lookback_days": {
+                                "type": "integer",
+                                "title": "Integrated deforestation alerts lookback days",
+                                "default": 15,
+                                "maximum": 30,
+                                "minimum": 1,
+                                "description": "Number of days to look back for integrated deforestation alerts."
+                            },
+                            "integrated_alerts_lowest_confidence": {
+                                "allOf": [{
+                                    "$ref": "#/definitions/IntegratedAlertsConfidenceEnum"
+                                }],
+                                "title": "Integrated deforestation alerts lowest confidence",
+                                "default": "highest",
+                                "description": "Lowest confidence level to include in the connection."
+                            }
+                        }
+                    }
+                }]
+            })
 
 
 class GetDatasetAndGeostoresConfig(PullActionConfiguration):
