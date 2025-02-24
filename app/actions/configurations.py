ce35131d5280b0a19fc0647de0c663d4835ad5b3
Victor H. García <victorhugogarcia13d@gmail.com>
2025-02-24 01:40:20 -0600

    GUNDI-4040: GFW sub-actions support

--- app/actions/configurations.py
+++ app/actions/configurations.py
@@ -1,12 +1,33 @@
 import pydantic
-from .core import PullActionConfiguration, AuthActionConfiguration, ExecutableActionMixin
-from .gfwclient import IntegratedAlertsConfidenceEnum, NasaViirsFireAlertConfidenceEnum
+
+from datetime import datetime
+
+from app.actions.core import PullActionConfiguration, AuthActionConfiguration, ExecutableActionMixin
+from app.actions.gfwclient import AOIData, DatasetResponseItem, IntegratedAlertsConfidenceEnum, NasaViirsFireAlertConfidenceEnum
+from app.services.errors import ConfigurationNotFound
+from app.services.utils import find_config_for_action
+
 
 class AuthenticateConfig(AuthActionConfiguration, ExecutableActionMixin):
     email: str
     password: pydantic.SecretStr = pydantic.Field(..., format="password", 
                                                   title="Password",
                                                   description="Password for the Global Forest Watch account.")
+
+
+def get_auth_config(integration):
+    # Look for the login credentials, needed for any action
+    auth_config = find_config_for_action(
+        configurations=integration.configurations,
+        action_id="auth"
+    )
+    if not auth_config:
+        raise ConfigurationNotFound(
+            f"Authentication settings for integration {str(integration.id)} "
+            f"are missing. Please fix the integration setup in the portal."
+        )
+    return AuthenticateConfig.parse_obj(auth_config.data)
+
 
 class PullEventsConfig(PullActionConfiguration):
 
@@ -59,3 +80,28 @@ class PullEventsConfig(PullActionConfiguration):
         title="Force fetch",
         description="Force fetch even if in a quiet period."
     )
+
+
+class GetDatasetAndGeostoresConfig(PullActionConfiguration):
+    integration_id: str
+    pull_events_config: PullEventsConfig
+    auth_config: AuthenticateConfig
+    aoi_data: AOIData
+
+
+class GetNasaVIIRSFireAlertsForGeostoreID(PullActionConfiguration):
+    integration_id: str
+    geostore_id: str
+    auth_config: AuthenticateConfig
+    date_range: tuple[datetime, datetime]
+    lowest_confidence: NasaViirsFireAlertConfidenceEnum
+    dataset: DatasetResponseItem
+
+
+class GetIntegratedAlertsForGeostoreID(PullActionConfiguration):
+    integration_id: str
+    geostore_id: str
+    auth_config: AuthenticateConfig
+    date_range: tuple[datetime, datetime]
+    lowest_confidence: IntegratedAlertsConfidenceEnum
+    dataset: DatasetResponseItem
